8a723e267dc82a99abd980be68bcfbf177e67302
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2020-11-10 19:42:01 -0700

    cleaning

--- kannbann.server/server/controllers/ListController.js
+++ kannbann.server/server/controllers/ListController.js
@@ -1,5 +1,5 @@
 import BaseController from '../utils/BaseController'
-import { Auth0Provider } from '@bcwdev/auth0provider'
+// import { Auth0Provider } from '@bcwdev/auth0provider'
 import { listService } from '../services/ListService'
 
 export class ListController extends BaseController {
@@ -12,38 +12,6 @@ export class ListController extends BaseController {
   async create(req, res, next) {
     try {
       res.send(await listService.create(req.body))
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async(req, res, next) {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async(req, res, next) {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async(req, res, next) {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async(req, res, next) {
-    try {
-
     } catch (error) {
       next(error)
     }
