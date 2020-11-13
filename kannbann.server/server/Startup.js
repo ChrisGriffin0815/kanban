1b7eb1984e0da8c22211c3e1601bc9f1ab578357
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2020-11-13 15:47:16 -0700

    allowed all

--- kannbann.server/server/Startup.js
+++ kannbann.server/server/Startup.js
@@ -23,8 +23,8 @@ export default class Startup {
         contentSecurityPolicy: {
           directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", `https://${domain}/*`],
-            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+            scriptSrc: ["'self'", `https://${domain}/*`, '*'],
+            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'", '*'],
             imgSrc: ["'self'", '*.githubusercontent.com', '*.blob.core.windows.net'],
             connectSrc: ["'self'", `https://${domain}/oauth/token`, `https://${domain}/userinfo`],
             fontSrc: ["'self'", 'https://fonts.gstatic.com'],
