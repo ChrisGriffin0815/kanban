4bd0e47e79a5ed8c58590f7d1e1cc24d314d270e
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2020-11-13 15:16:30 -0700

    trying to build

--- kannbann.client/vue.config.js
+++ kannbann.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
-  transpileDependencies: ['@bcwdev/auth0provider-client'],
+  transpileDependencies: ['@bcwdev/auth0-vue'],
   publicPath: '/',
   outputDir: '../kannbann.server/client',
   productionSourceMap: false
