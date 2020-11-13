7ac659c2e3e9741ab6495886611d08f6397adf26
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2020-11-13 12:54:44 -0700

    vue.config fix

--- kannbann.client/vue.config.js
+++ kannbann.client/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  transpileDependencies: ['@bcwdev/auth0provider-client'],
   publicPath: '/',
   outputDir: '../kannbann.server/client',
   productionSourceMap: false
